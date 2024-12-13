4484ab2ee9f29ab081edff43a2e2c827454c74ca
mayank9650 <mayankbhandari9650@gmail.com>
2024-12-13 16:07:13 +0530

    Passed data to recipe component

--- src/app/receipe/receipe.component.ts
+++ src/app/receipe/receipe.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from './receipe.model';
 
 @Component({
   selector: 'app-receipe',
   templateUrl: './receipe.component.html',
-  styleUrl: './receipe.component.css'
+  styleUrl: './receipe.component.css',
 })
 export class ReceipeComponent {
 
+  selectedRecipe: Recipe;
+
+  onRecipeChange(selectedRecipe: Recipe){
+    console.log('onRecipeChange', selectedRecipe);
+    this.selectedRecipe = selectedRecipe
+  }
 }
--- src/app/receipe/recipe-list/recipe-list.component.ts
+++ src/app/receipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../receipe.model';
 
 @Component({
@@ -15,10 +15,17 @@ export class RecipeListComponent {
         'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505',
     },
     {
-      name: 'A Test Recipe',
-      description: 'This is simply a test',
+      name: 'Noodles Recipe',
+      description: 'This is sample noodles recipe',
       imagePath:
-        'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505',
+        'https://static.vecteezy.com/system/resources/previews/043/290/966/non_2x/fresh-spaghetti-with-pesto-and-cherry-tomatoes-photo.jpg',
     },
   ];
+
+  @Output() recipeClickedHandler = new EventEmitter<Recipe>();
+
+  onSelectedRecipeClick(selectedRecipe: Recipe) {
+    console.log(selectedRecipe);
+    this.recipeClickedHandler.emit(selectedRecipe);
+  }
 }
