5f95f1348d697fb4010e19917195b22b3f1bd8e1
mayank9650 <mayankbhandari9650@gmail.com>
2024-12-13 12:02:11 +0530

    Basic recipe and shopping list setup

--- src/app/receipe/recipe-list/recipe-list.component.ts
+++ src/app/receipe/recipe-list/recipe-list.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../receipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    {
+      name: 'A Test Recipe',
+      description: 'This is simply a test',
+      imagePath:
+        'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505',
+    },
+    {
+      name: 'A Test Recipe',
+      description: 'This is simply a test',
+      imagePath:
+        'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505',
+    },
+  ];
 }
