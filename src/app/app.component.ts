bb74d891e00e4c49b426fb17816525e1226e0617
mayank9650 <mayankbhandari9650@gmail.com>
2024-12-13 15:35:08 +0530

    Displaying recipe and shopping list conditionally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  showRecipe = true;
+
+  onHeaderTabClicked(clickedTab: string) {
+    if(clickedTab === 'recipe'){
+      this.showRecipe = true
+    } else {
+      this.showRecipe = false
+    }
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  selectedTab = '';
+  @Output() tabClicked: EventEmitter<string> = new EventEmitter<string>();
 
+  onTabClick(clickedTab: string) {
+    this.selectedTab = clickedTab
+    this.tabClicked.emit(clickedTab)
+  }
 }
