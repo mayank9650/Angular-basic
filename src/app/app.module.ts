9e174d1f7dcb21f9e3d104772ea6cbaa49adee4f
mayank9650 <mayankbhandari9650@gmail.com>
2024-12-13 10:59:19 +0530

    Folder structure added for project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeDetailComponent } from './receipe/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './receipe/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './receipe/recipe-list/recipe-list.component';
+import { ReceipeComponent } from './receipe/receipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    ReceipeComponent
   ],
   imports: [
     BrowserModule,
