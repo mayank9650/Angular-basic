5d8871d1f520941ddde58074c0e947ef09a8b838
mayank9650 <mayankbhandari9650@gmail.com>
2024-12-23 23:01:39 +0530

    Adding form input validation

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html'
+  templateUrl: './auth.component.html',
 })
-export class AuthComponent {}
+export class AuthComponent {
+  isLoginMode = false;
+  @ViewChild('authForm') authForm: NgForm;
+
+  onModeSwitch() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onFormSubmit(){
+    console.log('auth', this.authForm)
+  }
+}
